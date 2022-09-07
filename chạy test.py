b45c00025626e492360e889b0bd75f5f54a00089
luan <tranthanhnhu@gmail.com>
2022-09-07 09:28:49 +0700

    test

--- chạy test.py
+++ chạy test.py
@@ -1,13 +1,2 @@
-res = []
-
-leng = int(input())
-
-for i in range(leng):
-    n = int(input())
-    res.append(n)
-
-
-def evenNum(res):
-    return [c for c in res if c%2 == 0]
-
-print(evenNum(res))
+str = "bigcityboy"
+print (str[3:7:2])
